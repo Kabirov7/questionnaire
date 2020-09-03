04ddb289e06aab3e2a478e3b080f80565ac0adf4
Kabirov7 <arturkabirov16@gmail.com>
2020-09-03 20:28:22 +0600

    Updates

---
+++ precache-manifest.055a338429909a252130be0f4d6db986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3fdf45d329649e5631676947cf9c19",
+    "url": "/questionnaire/index.html"
+  },
+  {
+    "revision": "2422b817aa0a9337193a",
+    "url": "/questionnaire/static/css/main.c292d41b.chunk.css"
+  },
+  {
+    "revision": "2257814dd9ded27c030e",
+    "url": "/questionnaire/static/js/2.5b06a38b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/questionnaire/static/js/2.5b06a38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2422b817aa0a9337193a",
+    "url": "/questionnaire/static/js/main.d983614f.chunk.js"
+  },
+  {
+    "revision": "823d4fb9c9360cb8893e",
+    "url": "/questionnaire/static/js/runtime-main.7a8d77a9.js"
+  }
+]);
